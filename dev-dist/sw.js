1a11c4c2a7b3ff87034be10a19b94b5cfa71f93c
Tristan <tristanneal@pm.me>
2023-09-13 23:03:45 -0400

    IMG assets attempt

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8c4j1hi9evg"
+    "revision": "0.nko1820e7kg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,98 +1,97 @@
 import React from 'react'
-import { ExpensesContainer, GridContainer } from '../Styles/Styles'
+import { ExpensesContainer, TableContainer } from '../Styles/Styles'
 
 
 const Expenses = () => {
- 
   return (
     <>
       <ExpensesContainer>
         <h1>Bills</h1>
-        <GridContainer>
+        <TableContainer>
           <table>
             <tr>
-                <th>Type</th>
-                <th>Name</th>
-                <th>Amount</th>
-                <th>Due Date</th>
+              <th>Type</th>
+              <th>Name</th>
+              <th>Amount</th>
+              <th>Due</th>
             </tr>
             <tr>
-                <td>📱</td>
-                <td>Phone (Alice)</td>
-                <td>$30</td>
-                <td>24th</td>
+              <td>📱</td>
+              <td>Phone (Alice)</td>
+              <td>$30</td>
+              <td>24th</td>
             </tr>
             <tr>
-                <td>📱</td>
-                <td>iPhone </td>
-                <td>$10</td>
-                <td>24th</td>
+              <td>📱</td>
+              <td>iPhone </td>
+              <td>$10</td>
+              <td>24th</td>
             </tr>
             <tr>
-                <td>💳</td>
-                <td>Discover (Alice) </td>
-                <td>$95</td>
-                <td>17th</td>
+              <td>💳</td>
+              <td>Discover (Alice) </td>
+              <td>$95</td>
+              <td>17th</td>
             </tr>
             <tr>
-                <td>📺</td>
-                <td>Discord Nitro (Alice)</td>
-                <td>$10</td>
-                <td>10th</td>
+              <td>📺</td>
+              <td>Discord Nitro (Alice)</td>
+              <td>$10</td>
+              <td>10th</td>
             </tr>
             <tr>
-                <td>🌲</td>
-                <td>Amazon</td>
-                <td>$15</td>
-                <td>25th</td>
+              <td>🌲</td>
+              <td>Amazon</td>
+              <td>$15</td>
+              <td>25th</td>
             </tr>
             <tr>
-                <td>💡</td>
-                <td>Electric</td>
-                <td>$88</td>
-                <td>25th</td>
+              <td>💡</td>
+              <td>Electric</td>
+              <td>$88</td>
+              <td>25th</td>
             </tr>
             <tr>
-                <td>📶</td>
-                <td>Internet</td>
-                <td>$85</td>
-                <td>26th</td>
+              <td>📶</td>
+              <td>Internet</td>
+              <td>$85</td>
+              <td>26th</td>
             </tr>
             <tr>
-                <td>📺</td>
-                <td>YouTube</td>
-                <td>$12</td>
-                <td>27th</td>
+              <td>📺</td>
+              <td>YouTube</td>
+              <td>$12</td>
+              <td>27th</td>
             </tr>
             <tr>
-                <td>📺</td>
-                <td>Hulu+Disney</td>
-                <td>$23</td>
-                <td>6th</td>
+              <td>📺</td>
+              <td>Hulu+Disney</td>
+              <td>$23</td>
+              <td>6th</td>
             </tr>
             <tr>
-                <td>📺</td>
-                <td>HBO</td>
-                <td>$7.99</td>
-                <td>9th</td>
+              <td>📺</td>
+              <td>HBO</td>
+              <td>$7.99</td>
+              <td>9th</td>
             </tr>
             <tr>
-                <td>✒️</td>
-                <td>Inkdrop (Tristan)</td>
-                <td>$5</td>
-                <td>12th</td>
+              <td>✒️</td>
+              <td>Inkdrop (Tristan)</td>
+              <td>$5</td>
+              <td>12th</td>
             </tr>
             <tr>
-                <td>🏋🏼‍♂️</td>
-                <td>Youfit </td>
-                <td>$30</td>
-                <td>16th</td>
+              <td>🏋🏼‍♂️</td>
+              <td>Youfit </td>
+              <td>$30</td>
+              <td>16th</td>
             </tr>
             <tr>
-                <td>🥖</td>
-                <td>Groceries </td>
-                <td>$100</td>
-                <td>♾️</td>
+              <td>🥖</td>
+              <td>Groceries </td>
+              <td>$100</td>
+              <td>♾️</td>
             </tr>
           </table>
           <table>
@@ -105,7 +104,7 @@ const Expenses = () => {
               <td>$236</td>
             </tr>
           </table>
-        </GridContainer>
+        </TableContainer>
       </ExpensesContainer>
     </>
   )
